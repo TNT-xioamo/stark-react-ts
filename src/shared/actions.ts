c7ff39ceadc26612243b0216815c7a80a660dd03
xingliu <liuxing@easthome>
2023-02-27 14:17:56 +0800

    资源引用

---
+++ src/shared/actions.ts
@@ -0,0 +1,8 @@
+ // 共享
+import { initGlobalState, MicroAppStateActions } from 'qiankun'
+
+const initialState = {}
+
+const actions: MicroAppStateActions = initGlobalState(initialState)
+
+export default actions
