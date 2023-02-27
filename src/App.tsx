6222881debad159be2c9a32b6ae28426f71fbfa7
xingliu <liuxing@easthome>
2023-02-27 13:44:23 +0800

    strictStyleIsolation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import logo from './logo.svg'
+import { Link, BrowserRouter } from 'react-router-dom'
 
 
 function App() {
   return (
-    <div className="App">首页</div>
+    <div className="App">
+      <BrowserRouter>
+        <h3><Link to="/vue-quality-education">微前端Vue</Link></h3>
+      </BrowserRouter>
+      <div id='vue-quality-education' />
+    </div>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,24 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './style/index.css'
 
+import { registerMicroApps, start } from 'qiankun'
+
+registerMicroApps([
+  {
+    name: 'vue-quality-education',
+    container: '#vue-quality-education',
+    entry: 'http://localhost:3100/',
+    activeRule: '/vue-quality-education',
+  }
+])
+// 启动
+start({
+  sandbox: {
+    // 样式隔离特性
+    experimentalStyleIsolation: true,
+  }
+})
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
