a7e29f15b08839ec71a0cd815362b03df2d93f3d
xingliu <liuxing@easthome>
2023-02-27 16:36:29 +0800

    init

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,9 +1,26 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
+import { Layout, Menu, theme } from 'antd'
+
+import { Link, BrowserRouter } from 'react-router-dom'
+
+const { Header, Sider, Content, Footer } = Layout
+
 
 export default memo(function JMSLayout () {
+  const [collapsed, setCollapsed] = useState(false)
+  const { token: { colorBgContainer }, } = theme.useToken()
   return (
-    <>
-      123123
-    </>
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}></Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }} >
+          <BrowserRouter><Link to="/vue-quality-education">微前端Vue</Link></BrowserRouter>
+        </Header>
+        <Content style={{ margin: '24px 16px 0' }}>
+          <BrowserRouter><div id='vue-quality-education' /></BrowserRouter>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
   )
 })
