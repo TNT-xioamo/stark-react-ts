f1a4fe9d18715db87dc12ce9f5d1edd0df752861
xingliu <liuxingcoco@iCloud.com>
2023-03-09 10:50:01 +0800

    RouterGurad

--- src/hooks/userRouterGurad.ts
+++ src/hooks/userRouterGurad.ts
@@ -1,7 +1,7 @@
 import { message } from 'antd'
 import { useEffect } from 'react'
 import { useLocation, useRoutes, Location, useNavigate, NavigateFunction } from 'react-router-dom'
-
+import { sStorage } from '@/utils'
 interface RouteObject {
   caseSensitive?: boolean;
   children?: RouteObject[];
@@ -29,6 +29,23 @@ function guard(location: Location, navigate: NavigateFunction,  routes: RouteObj
   if (!routedetail) return false // navigate('/404')
   // 如果需要权限验证
   if (routedetail.auth) {
-    const token = localStorage.getItem('stark_token')
+    const token =  sStorage.get('stark_token')
+    if (!token) {
+      message.warning('请登录')
+      navigate(-1)
+      return false
+    }
   }
+  return true
+}
+
+export const RouterGurad = (routes:any) => { //  :RouteObject[]
+  const location = useLocation()
+  const navigate = useNavigate()
+  useEffect(() => {
+    guard(location, navigate, routes)
+  }, [location, navigate, routes])
+  // document.documentElement.scrollTo(0, 0)
+  const Route = useRoutes(routes)
+  return Route
 }
