22c65049e2cd148b183ef8f351035f3be5de37a8
xingliu <liuxingcoco@iCloud.com>
2023-03-23 16:59:18 +0800

    useDispatch

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import React, { memo, MouseEvent, useState } from 'react'
 
 import { useSelector, useDispatch } from 'react-redux'
 import { SET_SETTINGS_FOLD_ICON } from '@/store/reducers/settings'
@@ -7,24 +7,39 @@ import { Layout, theme } from 'antd'
 import JMSLayoutContent from '@/layout/content'
 import JMSResizebar from './sider/sider-resizebar'
 import { DragAside } from './sider/sider-style'
-const { Header, Sider, Content, Footer } = Layout
+const { Sider, Content,  } = Layout
 
 export default memo(function JMSLayout () {
   const { menuSide, foldIcon, themes, collapse }: any = useSelector((state: any) => state?.settings)
+  const [draggable, setDraggable] = useState(false)
 
   const dispatchFoldIcon = useDispatch()
-
   const { token: { colorBgContainer }, } = theme.useToken()
 
   const handleSetIsHovered = (status: Boolean) => {
     dispatchFoldIcon(SET_SETTINGS_FOLD_ICON(status))
   }
   
+  const handleMouseDown = (e: MouseEvent) => {
+    setDraggable(true)
+    console.log('handleMouseDown', e)
+  }
+
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!draggable) return
+    console.error('handleMouseMove', e)
+  }
+
+  const handleMouseUp = (e: MouseEvent) => {
+    setDraggable(false)
+    console.error('handleMouseUp', e)
+  }
+
   return (
     <Layout>
       <Sider className="app-side" trigger={null} theme={themes} width={ menuSide } collapsible collapsed={collapse}  onMouseEnter={() => handleSetIsHovered(true)} onMouseLeave={() => handleSetIsHovered(false)}>
         { foldIcon ? <JMSResizebar /> : '' }
-        <DragAside />
+        <DragAside onMouseDown={event => handleMouseDown(event)}  onMouseMove={event => handleMouseMove(event)} onMouseUp={event => handleMouseUp(event)} />
       </Sider>
       <Layout>
         <Content><JMSLayoutContent /></Content>
