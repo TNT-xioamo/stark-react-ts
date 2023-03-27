02431533dd3ecee429a2f5894c42a32e3164366a
xingliu <liuxingcoco@iCloud.com>
2023-03-27 13:24:15 +0800

    transform

--- src/views/homePopoverModule/module-style.ts
+++ src/views/homePopoverModule/module-style.ts
@@ -4,11 +4,18 @@ export const UserInfo = styled.div `
   width: 100%;
   position: sticky;
   .sys-logo {
+    cursor: pointer;
     .l-s {
       img {
         width: 105px;
         height: 28px;
       }
+    }
+    .user-space {
+      width: 20px;
+      height: 20px;
+      display: inline-block;
+      transform: translateX(-150%);
     }
   }
   .index-module_right {
@@ -24,7 +31,7 @@ export const UserInfo = styled.div `
     .avatar {
       width: 32px;
       height: 32px;
-      padding-left: 8px;
+      margin-left: 8px;
       &:hover {
         cursor: pointer;
         background: var(--sk-yuque-grey-300);
--- src/views/homePopoverModule/module-user-info.tsx
+++ src/views/homePopoverModule/module-user-info.tsx
@@ -1,19 +1,18 @@
 import React, { memo } from 'react'
 import { Tooltip, Avatar } from 'antd'
 import { UserInfo } from './module-style'
-import { BellOutlined, UserOutlined } from '@ant-design/icons'
+import { BellOutlined, UserOutlined, DownOutlined } from '@ant-design/icons'
 
 export default memo(function JMSUserInfo(props: any) {
   return (
     <>
       <UserInfo className='f-c-s-b sidebar-user-info'>
-        <div className='sys-logo'>
+        <div className='sys-logo f-c-s'>
           <span className='l-s'><img src={props.logo} alt="stark" /></span>
+          <span className='user-space'><DownOutlined /></span> 
         </div>
         <div className='index-module_right f-c-s-b'>
-          <div className='message flex-center'>
-            <Tooltip title="消息中心" overlayStyle={{ fontSize: 12 }}><BellOutlined /></Tooltip>
-          </div>
+          <div className='message flex-center'><Tooltip title="消息中心" overlayStyle={{ fontSize: 12 }}><BellOutlined /></Tooltip></div>
           <div className='avatar flex-center'><Avatar size={24} icon={<UserOutlined />} /></div>
         </div>
       </UserInfo>
